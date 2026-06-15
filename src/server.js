db77897e9e3fc24becce6827af87116e6c3bc65f
vaishnavi manikeri <155414266+VaishnaviManikeri@users.noreply.github.com>
2026-06-15 15:44:39 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import "dotenv/config";
 import { app } from "./app.js";
 import { connectDatabase } from "./config/database.js";
 
-const port = Number(process.env.PORT || 5000);
+const port = Number(process.env.PORT || 5030);
 
 async function startServer() {
   try {
