4f9d41d973067bb7d32a3cc7acefcd450e7280e4
vaishnavi manikeri <155414266+VaishnaviManikeri@users.noreply.github.com>
2026-06-15 16:01:03 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ export const app = express();
 app.use(helmet());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "http://mandkecollege.com",
     credentials: true,
   })
 );
